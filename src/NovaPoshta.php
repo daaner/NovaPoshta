d9a8b200eaf5ac855fbba5164463c40f0bee8c4f
Daan <daan@ukr.net>
2021-04-16 22:14:24 +0300

    Fix

--- src/NovaPoshta.php
+++ src/NovaPoshta.php
@@ -30,7 +30,7 @@ class NovaPoshta implements NovaPoshtaInterface
     /**
      * @return string
      */
-    public function getApi()
+    public function getApi():string
     {
         if (! $this->api) {
             $this->api = config('novaposhta.api_key');
@@ -42,7 +42,7 @@ class NovaPoshta implements NovaPoshtaInterface
     /**
      * @param string $api
      */
-    public function setApi($api)
+    public function setApi(string $api):self
     {
         $this->api = $api;
 
@@ -56,25 +56,22 @@ class NovaPoshta implements NovaPoshtaInterface
      * @param bool $auth
      * @return array
      */
-    public function getResponse($model, $calledMethod, $methodProperties, $auth = true)
+    public function getResponse(string $model, string $calledMethod, array $methodProperties, bool $auth = true):array
     {
         $url = $this->url.'/'.$model.'/'.$calledMethod;
         $body = [];
         $info = '';
 
+        $body['modelName'] = $model;
+        $body['calledMethod'] = $calledMethod;
+        $body['methodProperties'] = $methodProperties;
+
         if ($auth) {
             $body['apiKey'] = $this->api;
-            $body['modelName'] = $model;
-            $body['calledMethod'] = $calledMethod;
-            $body['methodProperties'] = $methodProperties;
-        } else {
-            $body['modelName'] = $model;
-            $body['calledMethod'] = $calledMethod;
-            $body['methodProperties'] = $methodProperties;
         }
 
-        $response = Http::timeout(config('turbosms.http_response_timeout', 3))
-            ->retry(config('turbosms.http_retry_max_time', 2), config('turbosms.http_retry_delay', 200))
+        $response = Http::timeout(config('novaposhta.http_response_timeout', 3))
+            ->retry(config('novaposhta.http_retry_max_time', 2), config('novaposhta.http_retry_delay', 200))
             ->withHeaders([
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
